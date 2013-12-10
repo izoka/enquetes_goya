98da93ac3c8fbd7489dfe6f11412120dcf4c9db2
Eric <eric.blineau@gmail.com>
2013-12-10 14:41:51 +0100

    ajout accueil

---
+++ src/Enquetes/MainBundle/Controller/AccueilController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Enquetes\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class AccueilController extends Controller
+{
+    public function indexAction()
+    {
+        $user = new Utilisateur();
+        $form = $this->createForm(new UtilisateurType(),$user);
+        $em = $this->getDoctrine()->getManager();
+        
+        $formHandler = new FormHandler($form,$this->get('request'),$em);
+        
+        if($formHandler->process()){
+             return $this->redirect
+                ($this->generateUrl('enquetes_main_accueil',
+                        array('id'=>$user->getId() ) ) );
+        }
+        
+        return $this->render('EnquetesMainBundle:Accueil:accueil.html.twig',
+                array('form'=>$form->createView()));
+        
+    }
+}
