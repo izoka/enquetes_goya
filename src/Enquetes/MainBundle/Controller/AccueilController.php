c7fc5b70310aabe54723da16dc0728f3067d1bde
Admin <maximechampeval@gmail.com>
2013-12-11 11:28:14 +0100

    Modif login

--- src/Enquetes/MainBundle/Controller/AccueilController.php
+++ src/Enquetes/MainBundle/Controller/AccueilController.php
@@ -6,11 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Enquetes\MainBundle\Entity\Utilisateur;
 use Enquetes\MainBundle\Form\UtilisateurType;
 use Enquetes\MainBundle\Form\FormHandler;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class AccueilController extends Controller
 {
     public function indexAction()
     {
+
+        
+    // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+      return $this->redirect($this->generateUrl('enquetes_main_creation'));
+    }
+
+    $request = $this->getRequest();
+    $session = $request->getSession();
+
+    // On vérifie s'il y a des erreurs d'une précédente soumission du formulaire
+    if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+      $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+    } else {
+      $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+      $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+    }
+
+
         $user = new Utilisateur();
         $form = $this->createForm(new UtilisateurType(),$user);
         $em = $this->getDoctrine()->getManager();
@@ -22,9 +42,14 @@ class AccueilController extends Controller
                 ($this->generateUrl('enquetes_main_accueil',
                         array('id'=>$user->getId() ) ) );
         }
+    
+        
         
         return $this->render('EnquetesMainBundle:Default:accueil.html.twig',
-                array('form'=>$form->createView()));
+                array('form'=>$form->createView(),
+      'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+      'error'         => $error,
+        ));
         
     }
 }
