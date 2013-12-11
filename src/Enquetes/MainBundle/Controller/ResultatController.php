47a03cd76740fe0d1bc5ae8306508c1b3cffe064
Farid <faridsahlimescam@voila.fr>
2013-12-11 15:23:22 +0100

    Debut page resultat

--- src/Enquetes/MainBundle/Controller/ResultatController.php
+++ src/Enquetes/MainBundle/Controller/ResultatController.php
@@ -2,23 +2,32 @@
 namespace Enquetes\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Enquetes\MainBundle\Form\FormHandler;
 
-class ResultatsController extends Controller
+class ResultatController extends Controller
 {
-     public function resultatAction()
-           
-    {
-        $form = $this->createForm(new ResultatType());
+     public function resultatAction() {
+         
+         if ($this->get('security.context')->isGranted('ROLE_USER')) {
+             
+             {
         $em = $this->getDoctrine()->getManager();
-        
-        $formHandler = new FormHandler($form,$this->get('request'),$em);
-        
-        if($formHandler->process()){
-             return $this->redirect
-                ($this->generateUrl('enquetes_main_resultat'));
-        }
-        
-          return $this->render('EnqueteMainBundle:Default:resultat.html.twig',
-                array('form'=>$form->createView()));
+        $enquetes = $em->getRepository('EnquetesMainBundle:Enquete')
+                ->getAllEnquete();
+//        var_dump( $enquetes);
+        return $this->render('EnquetesMainBundle:Default:resultat.html.twig',
+                array('enquetes'=>$enquetes));
+    }
+         }      
+          
+         }
 }
-}
+
+
+
+
+   
+       
+        
+
+    
