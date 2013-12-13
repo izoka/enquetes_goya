bc654189eced610e899b7b55bb625f7785a3e5b0
Admin <maximechampeval@gmail.com>
2013-12-13 13:26:51 +0100

    correction

--- src/Enquetes/MainBundle/Controller/CreationController.php
+++ src/Enquetes/MainBundle/Controller/CreationController.php
@@ -38,12 +38,13 @@ class CreationController extends Controller {
             return $this->redirect($this->generateUrl('enquetes_main_accueil'));
         }
     }
-
-    public function modifierAction(Enquete $enquete) {
+    
+        public function modifierAction(Enquete $enquete) {
         if ($this->get('security.context')->isGranted('ROLE_USER')) {
             $user = $this->get('security.context')->getToken()->getUser();
 //        $id_user = $user->getUserId();
 //        var_dump($id_user);
+
 //        $enquete->setIsactif(true);
             $form = $this->createForm(new EnqueteType(), $enquete);
 
@@ -52,36 +53,17 @@ class CreationController extends Controller {
             $formHandler = new FormHandler($form, $this->get('request'), $em);
 
 
-            $request = $this->get('request');
-            if ($request->getMethod() == 'POST') {
-                $form->bind($request);
-                    if ($form->isValid()) {
-
-                        $obj = $form->getData();
-
-                        $enquete = $this->getDoctrine()->getManager()
-                                ->getRepository("EnquetesMainBundle:Enquete")
-                                ->find($obj->getEnqueteId());
-
-                        $questionList = $obj->getQuestion();
-                        foreach ($questionList as $questionRow) {
-                            $question = new Question();
-                            $question->setLibelle($questionRow->getLibelle());
-                            $question->setEnqueteEnquete($enquete);
-                            $question->setTypeTypeDeQuestion($questionRow->getTypeTypeDeQuestion());
-                             
-                            $em->persist($question);
-                            $em->flush();
-                            
-                        }
-                    }
-                }
-
-                return $this->render('EnquetesMainBundle:Default:creation.html.twig', array('form' => $form->createView()));
-            } else {
-                return $this->redirect($this->generateUrl('enquetes_main_accueil'));
+            if ($formHandler->process()) {
+//             return $this->redirect
+//                ($this->generateUrl('enquetes_main_creation_step2',
+//                        array('id'=>$enquete->getEnqueteId() ) ) );
             }
+
+            return $this->render('EnquetesMainBundle:Default:creation.html.twig', array('form' => $form->createView()));
+        } else {
+            return $this->redirect($this->generateUrl('enquetes_main_accueil'));
         }
+    }
 
 //
 //    public function etape2Action($id)
@@ -122,5 +104,4 @@ class CreationController extends Controller {
 ////            $builder->add('eventType', 'entity', array('class' => 'Bundle:EventType', 'property' => 'eventName', 'required' => true));
 //        } 
 //    }
-    }
-    
+}
